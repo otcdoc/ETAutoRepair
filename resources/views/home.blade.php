8331df81e859dc607dd370e532562b7c66ee24ef
Inertiaddict <mfallon@otchest.com>
2016-09-10 20:33:51 -0400

    Changes to Home and Layout blades
    This is a good push to commit. 100%

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,21 +12,38 @@
         <!-- Wrapper for slides -->
         <div class="carousel-inner">
             <div class="item active">
-                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide One');"></div>
-                <div class="carousel-caption">
-                    <h2>Caption 1</h2>
-                </div>
+                <div class="fill" style="background-image:url('http://inertiaddict.com/images/WorkingonCar.jpg');"></div>
+                    <div id="banner">
+                        <div class="container">
+                            <div class="carousel-caption">
+                                <div id="caption-banner-box">
+                                    <h2>All Work Guaranteed!</h2>
+                                    <p>Call us to schedule an appointment today!</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
             </div>
             <div class="item">
-                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Two');"></div>
-                <div class="carousel-caption">
-                    <h2>Caption 2</h2>
-                </div>
+                <div class="fill" style="background-image:url('http://inertiaddict.com/images/lobby.jpg');"></div>
+                    <div id="banner">
+                        <div class="container">
+                            <div class="carousel-caption">
+                                <div id="caption-banner-box">
+                                    <h2>Experienced and Trusted</h2>
+                                    <p>Whether it's an emergency, or just scheduled maintenance, E&T has been in business for over 40 years, and knows how to treat you, your wallet, and your vehicle.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
             </div>
             <div class="item">
-                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Three');"></div>
+                <div class="fill" style="background-image:url('http://inertiaddict.com/images/sign.jpg');"></div>
                 <div class="carousel-caption">
-                    <h2>Caption 3</h2>
+                    <div id="caption-banner-box">
+                    <h2>Established and Tested</h2>
+                    <p>Since 1971 we have been the right choice for auto and light truck repair in Suffolk County.</p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -47,17 +64,17 @@
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">
-                    Welcome to Modern Business
+                    Welcome to E&T Auto Repair
                 </h1>
             </div>
             <div class="col-md-4">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        <h4><i class="fa fa-fw fa-check"></i> Bootstrap v3.3.7</h4>
+                        <h4><i class="fa fa-fw fa-check"></i> E&T stands for Experience and Trust</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
-                        <a href="#" class="btn btn-default">Learn More</a>
+                        <p>We are a family owned and operated repair shop with a keen focus on customer service and satisfaction. We employ both New York State and ASE certified technicians. Our professionalism sets us apart from the rest. Visit us for a complete line of maintenance and repair needs. A friendly, clean and efficient shop that strives to make our customers feel welcome, comfortable and satisfied with each repair. </p>
+                        <a href="/contact" class="btn btn-default" class="center-this">Contact Us!</a>
                     </div>
                 </div>
             </div>
@@ -68,7 +85,7 @@
                     </div>
                     <div class="panel-body">
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
-                        <a href="#" class="btn btn-default">Learn More</a>
+                        
                     </div>
                 </div>
             </div>
