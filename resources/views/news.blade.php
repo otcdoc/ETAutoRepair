adf70a449569d8eb1778a43e4711b93bee1108da
otcdoc <pvu@otchest.com>
2016-09-10 21:17:56 -0700

    Added Facebook to News
    Facebook server-side code is added.

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,51 +1,72 @@
 @extends('layout')
 @section('fbsdk')
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '1115550901858422',
-      xfbml      : true,
-      version    : 'v2.7'
-    });
-    FB.api('/194281337341129', function(response) {
-        console.log(response);
-    });
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-</script>
 @stop
 @section('content')
     <!-- Page Content -->
     <div class="container">
 
-        <!-- Page Heading -->
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Specials
-                    <small>Tune in for Amazing Deals</small>
+                <h1 class="page-header">News
+                    <small>Latest Updates from E&T Auto</small>
                 </h1>
+            </div>
+        </div>
+        <!-- /.row --><?php $count = 0; ?>
+
+        @foreach ($graphEdge as $graphNode)
+        @if (($count < 5)&&(isset($graphNode['message'])))
+        <!-- Blog Post Row -->
+        <div class="row">
+            <div class="col-md-1 text-center">
+            @if ($graphNode['type'] == 'photo')
+                <p><i class="fa fa-camera fa-4x"></i></p>
+            @elseif ($graphNode['type'] == 'link')
+                <p><i class="fa fa-link fa-4x"></i></p>
+            @elseif ($graphNode['type'] == 'status')
+                <p><i class="fa fa-pencil-square fa-4x"></i></p>
+            @elseif ($graphNode['type'] == 'video')
+                <p><i class="fa fa-film fa-4x"></i></p>
+            @elseif ($graphNode['type'] == 'offer')
+                <p><i class="fa fa-money fa-4x"></i></p>
+            @else
+                <p><i class="fa fa-comment fa-4x"></i></p>
+            @endif
+                <p>{{ $graphNode['created_time']->format('F d, Y') }}</p>
+            </div>
+            <div class="col-md-5">
+                @if (isset($graphNode['full_picture']))
+                <a href="blog-post.html">
+                    <img class="img-responsive img-hover" src="{{ $graphNode['full_picture'] }}" alt="" height="300" width="600">
+                </a>
+                @else
+                <a href="https://placeholdit.imgix.net/~text?txtsize=56&txt=600%C3%97300&w=600&h=300">
+                    <img class="img-responsive img-hover" src="https://placeholdit.imgix.net/~text?txtsize=56&txt=600%C3%97300&w=600&h=300" alt="">
+                </a>
+                @endif
+            </div>
+            <div class="col-md-6">
+                <p>Posted on {{ $graphNode['created_time']->format('F d \a\t g:ia') }}.
+                </p>
+                <p>{{ $graphNode['message'] }}</p>
+                <a class="btn btn-primary" href="https://www.facebook.com/{{ $graphNode['id'] }}">Read on Facebook <i class="fa fa-angle-right"></i></a>
+            </div>
+        </div>
+        <hr>
+        <!-- /.row --><?php $count++; ?>
+        
+        @endif
+        @endforeach
+
+        <!-- Facebook Link -->
+        <div class="row">
+            <div class="col-lg-12">
+                <p>For more news, please visit our <a href="https://www.facebook.com/ET-Auto-Repair-Inc-194281337341129/">facebook page</a>!</p>
             </div>
         </div>
         <!-- /.row -->
 
-        <!-- Specials Coupons -->
-        <div class="row">
-            <div class="col-lg-12">
-                <object id="specials" style="width:100%; height: 600px;" data="{{ URL::asset('pdf/specials.pdf') }}"></object> 
-            </div>
-        </div>
+@stop
+@section('postjquery')
 
-        <div
-            class="fb-like"
-            data-share="true"
-            data-width="450"
-            data-show-faces="true">
-        </div>
 @stop
